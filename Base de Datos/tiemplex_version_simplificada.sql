8068f9e8267e5abb0a35b0acb9ce56377a012184
germanolivera <germanolivera.dg@gmail.com>
2025-07-01 18:47:26 -0300

    Subo la base de datos correcta

--- Base de Datos/tiemplex_version_simplificada.sql
+++ Base de Datos/tiemplex_version_simplificada.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 20-06-2025 a las 00:22:28
+-- Tiempo de generación: 01-07-2025 a las 23:44:30
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,51 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `negocio`
+-- Estructura de tabla para la tabla `rubros_sucursal`
 --
 
-CREATE TABLE `negocio` (
-  `id_negocio` int(11) NOT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `direccion` varchar(100) DEFAULT NULL,
-  `telefono` varchar(50) DEFAULT NULL,
-  `descripcion` varchar(200) DEFAULT NULL,
-  `fecha_alta` date DEFAULT NULL,
-  `id_rubro_negocio` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `negocio`
---
-
-INSERT INTO `negocio` (`id_negocio`, `nombre`, `direccion`, `telefono`, `descripcion`, `fecha_alta`, `id_rubro_negocio`) VALUES
-(11, 'Barbería El Rastro', 'Av. Corrientes 1234, CABA', '011-5555-1234', 'Especialistas en cortes clásicos y modernos para hombres.', '2025-06-14', NULL),
-(12, 'Barber Brothers', 'San Martín 456, Rosario', '0341-456-7890', 'Barbería premium con servicio de afeitado tradicional.', '2025-06-14', NULL),
-(13, 'Belleza Natural', 'Av. Santa Fe 789, CABA', '011-4444-5678', 'Centro de estética y cuidado facial con productos naturales.', '2025-06-14', NULL),
-(14, 'Glow Estética', 'Mitre 890, Córdoba', '0351-789-4561', 'Tratamientos de belleza avanzada y cuidado de la piel.', '2025-06-14', NULL),
-(15, 'Estetica Sol', 'Corrientes 1234', '1234567890', 'Nueva sucursal', '2025-06-17', 2);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `rubros_negocio`
---
-
-CREATE TABLE `rubros_negocio` (
-  `id_rubro_negocio` int(11) NOT NULL,
+CREATE TABLE `rubros_sucursal` (
+  `id_rubro_sucursal` int(11) NOT NULL,
   `nombre_rubro` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `rubros_negocio`
+-- Volcado de datos para la tabla `rubros_sucursal`
 --
 
-INSERT INTO `rubros_negocio` (`id_rubro_negocio`, `nombre_rubro`) VALUES
+INSERT INTO `rubros_sucursal` (`id_rubro_sucursal`, `nombre_rubro`) VALUES
 (1, 'Peluquería Masculina'),
-(2, 'Cosmética Femenina'),
-(3, 'Estética Unisex'),
+(2, 'Peluquería Femenina'),
+(3, 'Peluquería Unisex'),
 (4, 'Barbería Tradicional'),
-(5, 'Spa & Relax');
+(5, 'Servicio de Belleza Unisex');
 
 -- --------------------------------------------------------
 
@@ -78,7 +51,7 @@ INSERT INTO `rubros_negocio` (`id_rubro_negocio`, `nombre_rubro`) VALUES
 
 CREATE TABLE `servicios` (
   `id_servicio` int(11) NOT NULL,
-  `id_negocio` int(11) DEFAULT NULL,
+  `id_sucursal` int(11) DEFAULT NULL,
   `nombre` varchar(50) DEFAULT NULL,
   `descripcion` varchar(200) DEFAULT NULL,
   `duracion` int(11) DEFAULT NULL,
@@ -90,7 +63,7 @@ CREATE TABLE `servicios` (
 -- Volcado de datos para la tabla `servicios`
 --
 
-INSERT INTO `servicios` (`id_servicio`, `id_negocio`, `nombre`, `descripcion`, `duracion`, `precio`, `creacion_servicio`) VALUES
+INSERT INTO `servicios` (`id_servicio`, `id_sucursal`, `nombre`, `descripcion`, `duracion`, `precio`, `creacion_servicio`) VALUES
 (21, 11, 'Corte clásico', 'Corte de cabello estilo clásico con tijera y máquina.', 30, 3500.00, '2025-06-14'),
 (22, 11, 'Afeitado tradicional', 'Afeitado al ras con toalla caliente y navaja.', 20, 2500.00, '2025-06-14'),
 (23, 11, 'Perfilado de barba', 'Definición de barba y contornos con precisión.', 25, 3000.00, '2025-06-14'),
@@ -107,12 +80,38 @@ INSERT INTO `servicios` (`id_servicio`, `id_negocio`, `nombre`, `descripcion`, `
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `sucursal`
+--
+
+CREATE TABLE `sucursal` (
+  `id_sucursal` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `fecha_alta` date DEFAULT NULL,
+  `id_rubro_sucursal` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `sucursal`
+--
+
+INSERT INTO `sucursal` (`id_sucursal`, `nombre`, `direccion`, `telefono`, `descripcion`, `fecha_alta`, `id_rubro_sucursal`) VALUES
+(11, 'Pelos House San Telmo', 'Estados Unidos 1234, CABA', '011-5555-1234', 'Especialistas en cortes clásicos y modernos para hombres.', '2025-06-14', NULL),
+(12, 'Pelos House Rosario Centro', 'San Martín 456, Rosario', '0341-456-7890', 'Estética Integral y Peluquería Unisex', '2025-06-14', NULL),
+(13, 'Pelos House Recoleta', 'Av. Santa Fe 789, CABA', '011-4444-5678', 'Centro de estética y cuidado facial con productos naturales. Peluquería Femenina', '2025-06-14', NULL),
+(14, 'Pelos House Córdoba Centro', 'Mitre 890, Córdoba', '0351-789-4561', 'Peluqueria Unisex', '2025-06-14', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `turnos`
 --
 
 CREATE TABLE `turnos` (
   `id_turno` int(11) NOT NULL,
-  `id_negocio` int(11) NOT NULL,
+  `id_sucursal` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `id_empleado` int(11) DEFAULT NULL,
   `id_servicio` int(11) NOT NULL,
@@ -127,14 +126,15 @@ CREATE TABLE `turnos` (
 -- Volcado de datos para la tabla `turnos`
 --
 
-INSERT INTO `turnos` (`id_turno`, `id_negocio`, `id_cliente`, `id_empleado`, `id_servicio`, `fecha_solicitud`, `fecha_turno`, `hora_turno`, `estado`, `observaciones`) VALUES
-(21, 11, 13, 12, 21, '2025-06-14 19:55:09', '2025-06-14', '09:00:00', 'confirmado', NULL),
-(22, 11, 14, 11, 22, '2025-06-14 19:55:09', '2025-06-15', '10:00:00', 'confirmado', NULL),
+INSERT INTO `turnos` (`id_turno`, `id_sucursal`, `id_cliente`, `id_empleado`, `id_servicio`, `fecha_solicitud`, `fecha_turno`, `hora_turno`, `estado`, `observaciones`) VALUES
 (23, 12, 17, 15, 24, '2025-06-15 12:03:16', '2025-06-15', '11:00:00', 'confirmado', NULL),
 (24, 12, 18, 16, 25, '2025-06-15 12:03:16', '2025-06-16', '12:00:00', 'confirmado', NULL),
 (25, 13, 21, 19, 27, '2025-06-15 12:04:55', '2025-06-15', '14:00:00', 'confirmado', NULL),
 (26, 13, 22, 20, 28, '2025-06-15 12:04:55', '2025-06-16', '15:00:00', 'confirmado', NULL),
-(27, 14, 25, 23, 30, '2025-06-15 12:08:12', '2025-06-15', '16:00:00', 'confirmado', NULL);
+(34, 13, 13, 11, 22, '2025-06-29 23:59:47', '2025-06-30', '10:11:00', 'pendiente', 'Belleza natural'),
+(35, 11, 13, 19, 21, '2025-06-30 00:16:29', '2025-07-01', '10:00:00', 'confirmado', ''),
+(36, 13, 25, 19, 25, '2025-06-30 00:27:08', '2025-06-30', '11:20:00', 'pendiente', ''),
+(37, 12, 25, 19, 25, '2025-06-30 20:57:22', '2025-06-30', '21:09:00', 'pendiente', 'Llama para confirmar\nLlama para confirmar');
 
 -- --------------------------------------------------------
 
@@ -144,7 +144,7 @@ INSERT INTO `turnos` (`id_turno`, `id_negocio`, `id_cliente`, `id_empleado`, `id
 
 CREATE TABLE `usuarios` (
   `id_usuario` int(11) NOT NULL,
-  `id_negocio` int(11) DEFAULT NULL,
+  `id_sucursal` int(11) DEFAULT NULL,
   `nombre` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `telefono` varchar(50) DEFAULT NULL,
@@ -158,7 +158,7 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `id_negocio`, `nombre`, `email`, `telefono`, `password`, `tipo_usuario`, `rol_empleado`, `fecha_alta`) VALUES
+INSERT INTO `usuarios` (`id_usuario`, `id_sucursal`, `nombre`, `email`, `telefono`, `password`, `tipo_usuario`, `rol_empleado`, `fecha_alta`) VALUES
 (11, 11, 'Juan Barber', 'juan.barber@elrastro.com', '1155551111', 'pass123', 'empleado', 'supervisor', '2025-06-14'),
 (12, 11, 'Pedro Asistente', 'pedro.asistente@elrastro.com', '1155552222', 'pass123', 'empleado', 'consulta', '2025-06-14'),
 (13, 11, 'Carlos Gómez', 'carlos.gomez@gmail.com', '1155553333', 'pass123', 'cliente', NULL, '2025-06-14'),
@@ -181,31 +181,31 @@ INSERT INTO `usuarios` (`id_usuario`, `id_negocio`, `nombre`, `email`, `telefono
 --
 
 --
--- Indices de la tabla `negocio`
+-- Indices de la tabla `rubros_sucursal`
 --
-ALTER TABLE `negocio`
-  ADD PRIMARY KEY (`id_negocio`),
-  ADD KEY `id_rubro_negocio` (`id_rubro_negocio`);
-
---
--- Indices de la tabla `rubros_negocio`
---
-ALTER TABLE `rubros_negocio`
-  ADD PRIMARY KEY (`id_rubro_negocio`);
+ALTER TABLE `rubros_sucursal`
+  ADD PRIMARY KEY (`id_rubro_sucursal`);
 
 --
 -- Indices de la tabla `servicios`
 --
 ALTER TABLE `servicios`
   ADD PRIMARY KEY (`id_servicio`),
-  ADD UNIQUE KEY `uq_negocio_servicio_nombre` (`id_negocio`,`nombre`);
+  ADD UNIQUE KEY `uq_negocio_servicio_nombre` (`id_sucursal`,`nombre`);
+
+--
+-- Indices de la tabla `sucursal`
+--
+ALTER TABLE `sucursal`
+  ADD PRIMARY KEY (`id_sucursal`),
+  ADD KEY `id_rubro_negocio` (`id_rubro_sucursal`);
 
 --
 -- Indices de la tabla `turnos`
 --
 ALTER TABLE `turnos`
   ADD PRIMARY KEY (`id_turno`),
-  ADD KEY `id_negocio` (`id_negocio`),
+  ADD KEY `id_negocio` (`id_sucursal`),
   ADD KEY `id_cliente` (`id_cliente`),
   ADD KEY `id_empleado` (`id_empleado`),
   ADD KEY `id_servicio` (`id_servicio`);
@@ -216,23 +216,17 @@ ALTER TABLE `turnos`
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `id_negocio` (`id_negocio`);
+  ADD KEY `id_negocio` (`id_sucursal`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `negocio`
+-- AUTO_INCREMENT de la tabla `rubros_sucursal`
 --
-ALTER TABLE `negocio`
-  MODIFY `id_negocio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT de la tabla `rubros_negocio`
---
-ALTER TABLE `rubros_negocio`
-  MODIFY `id_rubro_negocio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `rubros_sucursal`
+  MODIFY `id_rubro_sucursal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
@@ -241,10 +235,16 @@ ALTER TABLE `servicios`
   MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
+-- AUTO_INCREMENT de la tabla `sucursal`
+--
+ALTER TABLE `sucursal`
+  MODIFY `id_sucursal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -257,22 +257,22 @@ ALTER TABLE `usuarios`
 --
 
 --
--- Filtros para la tabla `negocio`
---
-ALTER TABLE `negocio`
-  ADD CONSTRAINT `negocio_ibfk_1` FOREIGN KEY (`id_rubro_negocio`) REFERENCES `rubros_negocio` (`id_rubro_negocio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Filtros para la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`id_negocio`) REFERENCES `negocio` (`id_negocio`);
+  ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursal` (`id_sucursal`);
+
+--
+-- Filtros para la tabla `sucursal`
+--
+ALTER TABLE `sucursal`
+  ADD CONSTRAINT `sucursal_ibfk_1` FOREIGN KEY (`id_rubro_sucursal`) REFERENCES `rubros_sucursal` (`id_rubro_sucursal`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`id_negocio`) REFERENCES `negocio` (`id_negocio`),
+  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursal` (`id_sucursal`),
   ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `usuarios` (`id_usuario`),
   ADD CONSTRAINT `turnos_ibfk_3` FOREIGN KEY (`id_empleado`) REFERENCES `usuarios` (`id_usuario`),
   ADD CONSTRAINT `turnos_ibfk_4` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`);
@@ -281,7 +281,7 @@ ALTER TABLE `turnos`
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_negocio`) REFERENCES `negocio` (`id_negocio`);
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `sucursal` (`id_sucursal`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
